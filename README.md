eed679e4143dbfb5ef152c26f22d190606b64580
Grant Rettke <gcr@wisdomandwonder.com>
2018-03-13 00:13:13 -0500

    (nhncmnt) Creates prettify-everything.el.
    - Populated via `auto-insert': convenience
    - Corrected email
    - Set URL
    - Set Package-Requires
    - Generated README.md, currently empty

---
+++ README.md
@@ -0,0 +1,9 @@
+# prettify-everything - The prettify-symbols-mode library for concisely composing non-trivial prettifications
+
+*Author:* Grant Rettke <gcr@wisdomandwonder.com><br>
+*URL:* [https://github.com/grettke/prettify-everything](https://github.com/grettke/prettify-everything)<br>
+
+
+
+---
+Converted from `prettify-everything.el` by [*el2markdown*](https://github.com/Lindydancer/el2markdown).
---
+++ prettify-everything.el
@@ -0,0 +1,32 @@
+;;; prettify-everything.el --- The prettify-symbols-mode library for concisely composing non-trivial prettifications.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Grant Rettke
+
+;; Author: Grant Rettke <gcr@wisdomandwonder.com>
+;; Keywords: convenience
+;; URL: https://github.com/grettke/prettify-everything
+;; Package-Requires: ((emacs "25.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'prettify-everything)
+;;; prettify-everything.el ends here
